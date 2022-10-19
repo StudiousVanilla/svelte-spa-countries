14a8842543d57af3b0b5b48e0f9c87c69c70126b
Oisín Byrne <obyrne@cartrawler.com>
2022-10-19 10:05:10 +0100

    initial commit for deploy testing

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+
+
+</script>
+
+<main class="flex flex-col justify-center items-center">
+  <p class="text-8xl text-red-600 font-semibold">be nice</p>
+  <p class="text-8xl text-blue-600 font-semibold">Let's hit the Floor</p> <Counter/>
+  <img class="w-40 h-40" src="https://i1.sndcdn.com/avatars-000028998070-7cw994-t500x500.jpg" alt="">
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+    window.alert('I wanna see you dance mate!')
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
